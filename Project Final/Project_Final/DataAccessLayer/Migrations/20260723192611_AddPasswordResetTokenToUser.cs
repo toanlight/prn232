2c1c453379676d802ce91b28fce49150d550ff0d
toan <toanduong19704@gmail.com>
2026-07-24 02:26:48 +0700

    feat: add password reset token and expiry fields to User entity migration

---
+++ Project Final/Project_Final/DataAccessLayer/Migrations/20260723192611_AddPasswordResetTokenToUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordResetTokenToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetExpiry",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetToken",
+                table: "Users",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetExpiry",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetToken",
+                table: "Users");
+        }
+    }
+}
